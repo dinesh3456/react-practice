e89415e96d237b868cc2c7e72d306930e97ed56a
Dinesh <dinesh2952002@outlook.com>
2023-12-05 22:15:28 +0530

    implemented dark mode

--- src/components/About.js
+++ src/components/About.js
@@ -1,76 +1,68 @@
 import React, { useState } from "react";
 
 export default function About() {
-  const [myStyle, setMyStyle] = useState({
-    color: "white",
-    backgroundColor: "black",
-    border: "1px solid white",
-  });
-
-  const [btntext, setBtnText] = useState("Enable Dark Mode");
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const toggleStyle = () => {
-    if (myStyle.color === "white") {
-      setMyStyle({
-        color: "black",
-        backgroundColor: "white",
-      });
-      setBtnText("Enable Dark Mode");
-    } else {
-      setMyStyle({
-        color: "white",
-        backgroundColor: "black",
-      });
-      setBtnText("Enable Light Mode");
-    }
-};
-    return (
-      <div className="container" style={myStyle}>
-        <h1>About Us</h1>
-        <div className="accordion" id="accordionExample">
-          <div className="accordion-item">
-            <h2 className="accordion-header">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
-                aria-expanded="true"
-                aria-controls="collapseOne"
-              >
-                Accordion Item #1
-              </button>
-            </h2>
-            <div
-              id="collapseOne"
-              className="accordion-collapse collapse show"
-              data-bs-parent="#accordionExample"
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const myStyle = {
+    color: isDarkMode ? "white" : "black",
+    backgroundColor: isDarkMode ? "black" : "white",
+    border: "1px solid white",
+  };
+
+  const btntext = isDarkMode ? "Enable Light Mode" : "Enable Dark Mode";
+
+  return (
+    <div className="container" style={myStyle}>
+      <h1>About Us</h1>
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
             >
-              <div className="accordion-body" style={myStyle}>
-                <strong>This is the first item's accordion body.</strong> It is
-                shown by default, until the collapse plugin adds the appropriate
-                classNamees that we use to style each element. These classNamees
-                control the overall appearance, as well as the showing and
-                hiding via CSS transitions. You can modify any of this with
-                custom CSS or overriding our default variables. It's also worth
-                noting that just about any HTML can go within the{" "}
-                <code>.accordion-body</code>, though the transition does limit
-                overflow.
-              </div>
+              Accordion Item #1
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body" style={myStyle}>
+              <strong>This is the first item's accordion body.</strong> It is
+              shown by default, until the collapse plugin adds the appropriate
+              classNamees that we use to style each element. These classNamees
+              control the overall appearance, as well as the showing and hiding
+              via CSS transitions. You can modify any of this with custom CSS or
+              overriding our default variables. It's also worth noting that just
+              about any HTML can go within the <code>.accordion-body</code>,
+              though the transition does limit overflow.
             </div>
           </div>
         </div>
-        <div className="container my-3">
-          <button
-            onClick={toggleStyle}
-            type="button"
-            className="btn btn primary"
-            style={{ color: myStyle.color, backgroundColor: myStyle.backgroundColor}}
-          >
-            {btntext}
-          </button>
-        </div>
       </div>
-    );
-  
+      <div className="container my-3">
+        <button
+          onClick={toggleStyle}
+          type="button"
+          className="btn btn primary"
+          style={{
+            color: myStyle.color,
+            backgroundColor: myStyle.backgroundColor,
+          }}
+        >
+          {btntext}
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function NavBar() {
+  // State to track the current mode (light or dark)
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Function to toggle between light and dark mode
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  // Update the body background color based on the mode
+  useEffect(() => {
+    document.body.classList.toggle("dark-mode", isDarkMode);
+  }, [isDarkMode]);
+
+  // Dynamically set the navbar color based on the mode
+  const navbarClass = `navbar navbar-expand-lg ${
+    isDarkMode ? "navbar-dark bg-dark" : "navbar-light bg-light"
+  }`;
+
+  // Dynamically set the text color based on the mode
+  const brandClass = `navbar-brand ${isDarkMode ? "text-light" : "text-dark"}`;
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-        <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          React app
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                About
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-info" type="submit">
-              Search
-            </button>
-          </form>
+    <div>
+      <nav className={navbarClass}>
+      <div className="container-fluid">
+          <a className={brandClass} href="/">
+            React app
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+            </ul>
+          
+      </div>
+      <div className="form-check form-switch">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id="darkModeSwitch"
+          checked={isDarkMode}
+          onChange={toggleDarkMode}
+        />
+        <label className="form-check-label" htmlFor="darkModeSwitch">
+          Dark Mode
+        </label>
         </div>
       </div>
-    </nav>
+        </nav>
+      </div>
+      
+
   );
 }
