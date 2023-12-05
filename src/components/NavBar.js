3181d7dc8c19d5323bb6f345e5251a5b06284554
Dinesh <dinesh2952002@outlook.com>
2023-12-05 22:26:33 +0530

    changed the  application background color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,16 +3,32 @@ import React, { useState, useEffect } from "react";
 export default function NavBar() {
   // State to track the current mode (light or dark)
   const [isDarkMode, setIsDarkMode] = useState(false);
+  // State to track the selected background color
+  const [selectedColor, setSelectedColor] = useState(null);
 
   // Function to toggle between light and dark mode
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
+    setSelectedColor(null); // Reset selected color when toggling dark mode
   };
 
-  // Update the body background color based on the mode
+  // Function to handle background color change
+  const handleColorChange = (color) => {
+    if (selectedColor === color) {
+      // Deselect the color if it's already selected
+      setSelectedColor(null);
+    } else {
+      // Select the color
+      setSelectedColor(color);
+      setIsDarkMode(false); // Reset dark mode when changing background color
+    }
+  };
+
+  // Update the body background color based on the mode and selected color
   useEffect(() => {
     document.body.classList.toggle("dark-mode", isDarkMode);
-  }, [isDarkMode]);
+    document.body.style.backgroundColor = selectedColor || "";
+  }, [isDarkMode, selectedColor]);
 
   // Dynamically set the navbar color based on the mode
   const navbarClass = `navbar navbar-expand-lg ${
@@ -25,7 +41,7 @@ export default function NavBar() {
   return (
     <div>
       <nav className={navbarClass}>
-      <div className="container-fluid">
+        <div className="container-fluid">
           <a className={brandClass} href="/">
             React app
           </a>
@@ -53,24 +69,35 @@ export default function NavBar() {
                 </a>
               </li>
             </ul>
-          
-      </div>
-      <div className="form-check form-switch">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          id="darkModeSwitch"
-          checked={isDarkMode}
-          onChange={toggleDarkMode}
-        />
-        <label className="form-check-label" htmlFor="darkModeSwitch">
-          Dark Mode
-        </label>
+            <div className="form-check form-switch mx-2">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="darkModeSwitch"
+                checked={isDarkMode}
+                onChange={toggleDarkMode}
+              />
+              <label className="form-check-label" htmlFor="darkModeSwitch">
+                Dark Mode
+              </label>
+            </div>
+            {["red", "blue", "yellow", "green"].map((color) => (
+              <div key={color} className="form-check form-switch mx-2" >
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id={`${color}Switch`}
+                  checked={selectedColor === color}
+                  onChange={() => handleColorChange(color)}
+                />
+                <label className="form-check-label" htmlFor={`${color}Switch`}>
+                  {color.charAt(0).toUpperCase() + color.slice(1)}
+                </label>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-        </nav>
-      </div>
-      
-
+      </nav>
+    </div>
   );
 }
