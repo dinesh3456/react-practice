dc10b5e580161bcf009a3938cc50a0aca5ac5176
Dinesh <dinesh2952002@outlook.com>
2023-12-05 11:17:55 +0530

    video 10 completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 //import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 import About from "./components/About";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="container my-3">
+      {/* <div className="container my-3">
         <TextForm heading="Enter the text to modify it!" />
-      </div>
+      </div> */}
       <About />
     </>
   );
